741a4820c6a6c2bbe2d7d423cf46760f90a0cf16
bhimsur <bhimantoros@gmail.com>
2023-10-03 16:16:22 +0700

    feat: init commit

---
+++ src/main/java/io/github/bhimsur/quartzsandbox/QuartzSandboxApplication.java
@@ -0,0 +1,13 @@
+package io.github.bhimsur.quartzsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/bhimsur/quartzsandbox/QuartzSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bhimsur.quartzsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
